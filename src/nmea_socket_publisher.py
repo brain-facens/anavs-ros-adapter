c00875ab11eba39ecb9083e43d8bd967d820d4c9
Vinícius Frias A Leite <vini.fal2002@gmail.com>
2022-07-22 11:33:34 -0300

    Use Python2 for ROS compatibility

--- src/nmea_socket_publisher.py
+++ src/nmea_socket_publisher.py
@@ -1,22 +1,23 @@
-# coding: utf-8
+#!/usr/bin/env python2
 import socket
 import rospy
 
 from nmea_msgs.msg import Sentence
 
 rospy.init_node('nmea_messages')
-pub = rospy.Publisher('messages', Sentence)
+pub = rospy.Publisher('nmea_sentence', Sentence, queue_size=20)
 r = rospy.Rate(20)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.0.101', 6002))
 
-with s.makefile() as f:
-    while not rospy.is_shutdown():
-        msg = Sentence()
-        msg.sentence = f.readline()[:-1]
-        msg.header.frame_id = 'gps'
-        msg.header.stamp = rospy.Time.now()
-        pub.publish(msg)
-        r.sleep()
-        
+f = s.makefile()
+while not rospy.is_shutdown():
+    msg = Sentence()
+    msg.sentence = f.readline()[:-1]
+    msg.header.frame_id = 'gps'
+    msg.header.stamp = rospy.Time.now()
+    pub.publish(msg)
+    r.sleep()
+
+f.close()
