3755ef61b8732c9dac8884954ff825097898a8c5
Vinícius Frias A Leite <vini.fal2002@gmail.com>
2022-08-03 17:06:16 -0300

    Add tf2 broadcaster

---
+++ src/tf2_broadcaster.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+import pyproj
+import rospy
+import tf2_ros
+
+from geometry_msgs.msg import TransformStamped, Quaternion, Vector3
+from sensor_msgs.msg import NavSatFix
+
+WSG84 = 4326
+XY_METERS = 3857
+
+coord_transformer = pyproj.Transformer.from_crs(WSG84, XY_METERS)
+
+def broadcast_gnss(fix, gnss_name):
+    br = tf2_ros.TransformBroadcaster()
+    t = TransformStamped()
+
+    t.header.stamp = rospy.Time.now()
+    t.header.frame_id = "world"
+    t.child_frame_id = gnss_name
+
+    x, y = coord_transformer.transform(fix.latitude, fix.longitude)
+    t.transform.translation = Vector3(x, y, fix.altitude)
+
+    t.transform.rotation = Quaternion(w=1)
+
+    br.sendTransform(t)
+
+if __name__ == '__main__':
+    rospy.init_node('gnss_broadcaster')
+    gnss_name = rospy.get_param('gnss_name', default='gps')
+    rospy.Subscriber('/fix', NavSatFix, broadcast_gnss, callback_args=gnss_name)
+    rospy.spin()
+    
