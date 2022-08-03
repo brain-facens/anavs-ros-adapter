2e28f2571572f3574ea363e0921b35b433881757
Vinícius Frias A Leite <vini.fal2002@gmail.com>
2022-08-03 17:03:46 -0300

    Add parameters

--- src/nmea_socket_publisher.py
+++ src/nmea_socket_publisher.py
@@ -5,17 +5,20 @@ import rospy
 from nmea_msgs.msg import Sentence
 
 rospy.init_node('nmea_messages')
+IP_ADDRESS = rospy.get_param('ip_addr', default='192.168.0.101')
+PORT = rospy.get_param('port', default=6002)
+FRAME_ID = rospy.get_param('frame_id', default='gps')
 pub = rospy.Publisher('nmea_sentence', Sentence, queue_size=20)
-r = rospy.Rate(20)
+r = rospy.Rate(40)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.0.101', 6002))
+s.connect((IP_ADDRESS, PORT))
 
 f = s.makefile()
 while not rospy.is_shutdown():
     msg = Sentence()
     msg.sentence = f.readline()[:-2]
-    msg.header.frame_id = 'gps'
+    msg.header.frame_id = FRAME_ID
     msg.header.stamp = rospy.Time.now()
     pub.publish(msg)
     r.sleep()
