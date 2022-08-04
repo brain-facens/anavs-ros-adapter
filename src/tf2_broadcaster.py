bc39ba753584b1984d34a8f996179cee8a6ea298
Vinícius Frias A Leite <vini.fal2002@gmail.com>
2022-08-04 11:08:01 -0300

    Bring global variables to wrapper function

--- src/tf2_broadcaster.py
+++ src/tf2_broadcaster.py
@@ -6,13 +6,25 @@ import tf2_ros
 from geometry_msgs.msg import TransformStamped, Quaternion, Vector3
 from sensor_msgs.msg import NavSatFix
 
-WSG84 = 4326
-XY_METERS = 3857
+def my_broadcaster(fix, gnss_name):
+    WSG84 = 4326
+    XY_METERS = 3857
 
-coord_transformer = pyproj.Transformer.from_crs(WSG84, XY_METERS)
+    coord_transformer = pyproj.Transformer.from_crs(WSG84, XY_METERS)
 
-def broadcast_gnss(fix, gnss_name):
-    br = tf2_ros.TransformBroadcaster()
+    return broadcast_gnss(fix, gnss_name, coord_transformer)
+
+
+def broadcast_gnss(fix, gnss_name, coord_transformer, br = tf2_ros.TransformBroadcaster()):
+    """Broadcast GNSS data as TF2
+
+    :param fix: Topic for GNSS data
+    :type fix: str
+    :param gnss_name: Name to represent that TF2
+    :type gnss_name: str
+    :param br: Broadcaster object (used to initialize it only once), defaults to tf2_ros.TransformBroadcaster()
+    :type br: TransformBroadcaster, optional
+    """
     t = TransformStamped()
 
     t.header.stamp = rospy.Time.now()
@@ -29,6 +41,6 @@ def broadcast_gnss(fix, gnss_name):
 if __name__ == '__main__':
     rospy.init_node('gnss_broadcaster')
     gnss_name = rospy.get_param('gnss_name', default='gps')
-    rospy.Subscriber('/fix', NavSatFix, broadcast_gnss, callback_args=gnss_name)
+    rospy.Subscriber('/fix', NavSatFix, my_broadcaster, callback_args=gnss_name)
     rospy.spin()
     
