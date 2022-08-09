a267a1f4eccc42fb66697bf15d33d7edfa369342
Vinícius Frias A Leite <vini.fal2002@gmail.com>
2022-08-09 16:33:47 -0300

    Change publish rate

--- src/nmea_socket_publisher.py
+++ src/nmea_socket_publisher.py
@@ -5,11 +5,13 @@ import rospy
 from nmea_msgs.msg import Sentence
 
 rospy.init_node('nmea_messages')
-IP_ADDRESS = rospy.get_param('ip_addr', default='192.168.0.101')
-PORT = rospy.get_param('port', default=6002)
-FRAME_ID = rospy.get_param('frame_id', default='gps')
-pub = rospy.Publisher('nmea_sentence', Sentence, queue_size=20)
-r = rospy.Rate(40)
+
+IP_ADDRESS = rospy.get_param('ip_addr',  default='192.168.0.101')
+PORT       = rospy.get_param('port',     default=6002           )
+FRAME_ID   = rospy.get_param('frame_id', default='gps'          )
+
+publisher = rospy.Publisher('nmea_sentence', Sentence, queue_size=5)
+r = rospy.Rate(200)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((IP_ADDRESS, PORT))
@@ -20,7 +22,7 @@ while not rospy.is_shutdown():
     msg.sentence = f.readline()[:-2]
     msg.header.frame_id = FRAME_ID
     msg.header.stamp = rospy.Time.now()
-    pub.publish(msg)
+    publisher.publish(msg)
     r.sleep()
 
 f.close()
