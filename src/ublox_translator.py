b3215a13f1865f75223eae9dba524130e963bc4f
Vinícius Frias A Leite <vini.fal2002@gmail.com>
2022-09-08 14:43:51 -0300

    Add function to combine ublox messages into pose

---
+++ src/ublox_translator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Translates the information from ublox GNSS to pseudo-Mercator
+"""
+
+import numpy as np
+import pyproj
+import rospy
+from geometry_msgs.msg import Point, PoseStamped, Quaternion
+from ublox_msgs.msg import NavPOSLLH, NavVELNED
+
+WSG84 = 4326
+XY_METERS = 3857
+
+coord_transformer = pyproj.Transformer.from_crs(WSG84, XY_METERS)
+
+def combine(enu_pose: NavVELNED, llh_position: NavPOSLLH):
+    """Combines orientation information from ENU message with position from LLH
+
+    :param pose: ENU pose message
+    :type pose: PoseWithCovarianceStamped
+    :param position: LLH position message
+    :type position: PointStamped
+    """
+
+    # Creates a pose object and fills header and orientation from ENU
+    new_pose = PoseStamped()
+    new_pose.header = rospy.Time.now()
+    theta = np.deg2rad(enu_pose.heading) / 2
+    new_pose.pose.orientation = Quaternion(w=np.cos(theta), z=np.sin(theta))
+    
+    # Tranlates coordinates and adds position information from LLH
+    x, y = coord_transformer.transform(llh_position.lat * 1e-7, llh_position.lon * 1e-7)
+    new_pose.pose.position = Point(x, y, llh_position.height)
